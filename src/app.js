01faefda6b137979e9f1b7e921a0aa9511ae8eb4
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-10-29 04:30:10 +0000

    started the project and implemented the functionality

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'my', 'your', 'their'];
+  let adj = ['great', 'big', 'crazy', 'lazy'];
+  let noun = ['wizard', 'ninja', 'pirate'];
+  let domainNames = ['.com','.net','.org','.dev'];
+
+  let domainElement = document.querySelector("#domain")
+
+  domainElement.innerHTML = createDomain(pronoun, adj, noun, domainNames);
+
 };
+
+
+
+function createDomain (pronouns, adjectives, nouns, domains){
+  let html = '';
+  pronouns.forEach( p => {
+    adjectives.forEach( a => {
+      nouns.forEach ( n => {
+        domains.forEach ( d => {
+           console.log(`${p}${a}${n}${d}`);
+           html +=  `<li>${p}${a}${n}${d}</li>`
+           
+        })
+      })
+    })
+  })
+  return `<ul> ${html}</ul>`;
+}
