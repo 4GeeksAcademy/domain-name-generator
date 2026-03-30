7e2443c4dc0a5a2d9e1172a326ea1ae0fc162b0f
Alberto Oliveira <albertooliveirajaen@gmail.com>
2026-03-30 09:04:13 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our', 'my', 'your'];
+  let adj = ['great', 'big', 'super', 'cool'];
+  let noun = ['jogger', 'racoon', 'dev', 'hub'];
+  let extensions = ['.com', '.net', '.us', '.io', '.es'];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extensions.length; e++) {
+          
+          let domainName = pronoun[p] + adj[a] + noun[n] + extensions[e];
+          console.log(domainName);
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
