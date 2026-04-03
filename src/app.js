f5ec720d5455acfe3f6739337250e3e3bc24a249
Alberto Oliveira <albertooliveirajaen@gmail.com>
2026-04-03 09:33:54 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -10,18 +10,31 @@ window.onload = function() {
   let noun = ['jogger', 'racoon', 'dev', 'hub'];
   let extensions = ['.com', '.net', '.us', '.io', '.es'];
 
+  console.log("--- Inicio de generación con bucle FOR ---");
+  
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domainNameFor = pronoun[i] + adj[j] + noun[k] + extensions[l];
+          console.log("FOR: " + domainNameFor);
+        }
+      }
+    }
+  }
+
+  console.log("--- Inicio de generación con bucle FOREACH ---");
+  
   pronoun.forEach((p) => {
     adj.forEach((a) => {
       noun.forEach((n) => {
         extensions.forEach((e) => {
-          
-          let domainName = p + a + n + e;
-          
-          console.log(domainName);
+          let domainNameForEach = p + a + n + e;
+          console.log("FOREACH: " + domainNameForEach);
         });
       });
     });
   });
 
-  console.log("Generación de dominios con forEach completada con éxito.");
+  console.log("Proceso de generación de dominios finalizado con éxito.");
 };
